1a2966bbf9fa99141bf50c551aa49b9cb75f3643
Tom Badran <tom@badrunner.net>
2013-07-10 14:17:49 +0100

    Encoders now work

--- arduino/motor_test/motor_test.ino
+++ arduino/motor_test/motor_test.ino
@@ -10,34 +10,16 @@ const Motor m1 = {47, 46, 48, 49};
 const Motor m2 = {43, 42, 44, 45};
 
 typedef struct {
-    unsigned int index;
+    unsigned int Z;
     unsigned int A;
     unsigned int B;
 } Encoder;
 
-const Encoder e1 = {14, 15, 18};
-const Encoder e2 = {19, 20, 21};
+const Encoder e1 = {2, 18, 19};
+const Encoder e2 = {3, 20, 21};
 
-volatile unsigned int e1_steps = 0;
-volatile unsigned int e2_steps = 0;
-
-void setup() {
-    Serial.begin(9600);
-
-    pinMode(m1.clock, OUTPUT);
-    pinMode(m1.sync, OUTPUT);
-    pinMode(m1.dir, OUTPUT);
-    //pinMode(m1pins[m_home_pin], INPUT);
-
-    pinMode(m2.clock, OUTPUT);
-    pinMode(m2.sync, OUTPUT);
-    pinMode(m2.dir, OUTPUT);
-    //pinMode(m2pins[m_home_pin], INPUT);
-     
-    digitalWrite(m1.sync, HIGH);
-    digitalWrite(m2.sync, HIGH);
-    Serial.println("Listening for status changes ...");
-}
+volatile long e1_steps = 0;
+volatile long e2_steps = 0;
 
 void motor_step(unsigned int pin) {
     digitalWrite(pin, HIGH);
@@ -76,7 +58,66 @@ void turn(unsigned int motor, unsigned int direct, unsigned int steps) {
         delayMicroseconds(50);
     }
  
+    delay(10);
+    Serial.print("Encoder 1: ");
+    Serial.println(e1_steps);
+    Serial.print("Encoder 2: ");
+    Serial.println(e2_steps);
+
     digitalWrite(m.sync, HIGH);
+}
+
+void doEncode1() {
+    int A = digitalRead(e1.A);
+    int B = digitalRead(e1.B);
+
+    e1_steps += A == B ? 1 : -1;
+    
+    // Serial.print("1: ");
+    // Serial.print(A);
+    // Serial.print('\t');
+    // Serial.println(B);
+}
+
+void doEncode2() {
+    int A = digitalRead(e2.A);
+    int B = digitalRead(e2.B);
+
+    e2_steps += A == B ? 1 : -1;
+    // Serial.print("2: ");
+    // Serial.print(A);
+    // Serial.print('\t');
+    // Serial.println(B);
+}
+
+void setup() {
+    Serial.begin(9600);
+
+    pinMode(m1.clock, OUTPUT);
+    pinMode(m1.sync, OUTPUT);
+    pinMode(m1.dir, OUTPUT);
+    pinMode(m1.home, INPUT);
+
+    pinMode(m2.clock, OUTPUT);
+    pinMode(m2.sync, OUTPUT);
+    pinMode(m2.dir, OUTPUT);
+    pinMode(m2.home, INPUT);
+     
+    pinMode(e1.Z, INPUT);
+    pinMode(e1.A, INPUT);
+    pinMode(e1.B, INPUT);
+
+    pinMode(e2.Z, INPUT);
+    pinMode(e2.A, INPUT);
+    pinMode(e2.B, INPUT);
+
+    digitalWrite(m1.sync, HIGH);
+    digitalWrite(m2.sync, HIGH);
+
+    attachInterrupt(0, doEncode1, CHANGE);
+    attachInterrupt(1, doEncode2, CHANGE);
+
+    Serial.println("Listening for status changes ...");
 }
 
 void loop() {
