cc61c9d93613f71787e1026f8d51cc7be14ac959
Tom Badran <tom@badrunner.net>
2013-08-22 10:20:00 +0100

    Implemented sun goto and tracking

--- solar_drive.py
+++ solar_drive.py
@@ -22,6 +22,7 @@ class SolarDriverApp(QtGui.QApplication):
         timer.start(300)
 
         ui.trackButton.pressed.connect(self.track)
+        ui.findSun.pressed.connect(self.find_sun)
         ui.zeroReturn.pressed.connect(self.return_to_zero)
 
         ui.raLeft.pressed.connect(self.raLeft)
@@ -63,12 +64,12 @@ class SolarDriverApp(QtGui.QApplication):
     def raLeft(self):
         arc = self.ui.calArcSec.value()
         solar.adjust_ra(-arc)
-        self._ra -= arc
+        self._ra -= arc * 15
 
     def raRight(self):
         arc = self.ui.calArcSec.value()
         solar.adjust_ra(arc)
-        self._ra += arc
+        self._ra += arc * 15
 
     def decLeft(self):
         arc = self.ui.calArcSec.value()
@@ -87,56 +88,79 @@ class SolarDriverApp(QtGui.QApplication):
             return
         logging.info('Tracking Sun Commencing')
         self.tracking = True
-        #self.find_sun()
+        self.find_sun()
         self.start_tracking()
 
-    def find_sun(self):
-        dec = (90. - self.ui.latitude.value()) * 3600
+    def sun_ra(self):
         mst = self.mst()
-        ra = timedelta(hours=mst.hour - 12, minutes=mst.minute, seconds=mst.second).total_seconds()
+        return timedelta(hours=mst.hour - 12, minutes=mst.minute, seconds=mst.second).total_seconds()
+
+    def sun_dec(self):
+        return (90. - self.ui.latitude.value()) * 3600
+
+    def find_sun(self):
+        dec = self.sun_dec()
+        ra = self.sun_ra()
 
         logging.info('Sun at: {} seconds {} arcsec'.format(ra, dec))
+
         solar.adjust_dec(dec - self._dec)
-        solar.adjust_ra_sec(ra - self._ra)
         self._dec = dec
-        self._ra = ra
+
+        while abs(ra - self._ra) > 5:
+            solar.adjust_ra_sec(ra - self._ra)
+            self._ra = ra
+            ra = self.sun_ra()
 
     def start_tracking(self):
-        # Track Loop
-        start = datetime.utcnow()
-        start_enc = solar.current_position(solar.Devices.body)
-        start_ra = self._ra
+        # Get sun position
+        # Add any calibraion
+        # If far slew to location, otherwise smooth track
         while self.tracking:
-            time_tracked = 0
-            enc_tracked = 0
+            # Compensate for any fine tuning of declination
+            dec = self._dec
+            target = self.sun_dec() + self.ui.decAdjust.value()
+            if abs(target - dec) > solar.ARCSEC_PER_ENC:
+                solar.adjust_dec(target - dec)
+                self._dec = target
+                logging.debug('Compensating declination adjustment')
 
-            # Track in 3 second bursts to allow for cancelling
-            while(time_tracked < 3.0):
+            # Now track along RA
+            ra = self._ra
+            target = self.sun_ra() + self.ui.raAdjust.value()
+
+            # If the gap has become large, slew
+            if abs(target - ra) > solar.SEC_PER_ENC:
+                solar.adjust_ra_sec(target - ra)
+                self._ra = target
+                logging.debug('Compensating for RA adjustment')
+
+            # Otherwise smoothly track for at least 3 seconds
+            time_tracked = 0
+            start = datetime.utcnow()
+            enc_tracked = 0
+            enc_start = solar.current_position(solar.Devices.body)
+            while(time_tracked < 3):
                 now = datetime.utcnow()
                 dt = (now - start).total_seconds()
-
                 enc_expected = math.floor(dt / solar.SEC_PER_ENC)
-                enc_error = enc_expected - (enc_tracked - start_enc)
+                enc_error = enc_expected - enc_tracked
+                turns = (dt - time_tracked) // solar.SEC_PER_STEP
 
-                turns = (dt - time_tracked) / solar.SEC_PER_STEP
-
-                if enc_error > 1:
-                    turns += (solar.STEPS_PER_ENC - 1) * enc_error
-                elif enc_error > 0:
+                if enc_error > 0:
                     turns += solar.SLIP_FACTOR
                 elif enc_error < 0:
-                    turns = -1
+                    turns = 0
 
                 if turns > 0:
                     solar.Telescope().send_command('T{}{}{}'.format(solar.Devices.body, solar.Directions.clockwise, int(turns)))
-                    enc_tracked = int(solar.Telescope().readline())
-                    logging.debug('Elapsed: {:6.2f} Turns: {:5.2f} Error: {}'.format(dt, turns, int(enc_error)))
+                    enc_tracked += int(solar.Telescope().readline()) - enc_start
+                    time_tracked = dt
+                    logging.debug('Micro Steps: {:5.2f} Encoder Error: {}'.format(turns, int(enc_error)))
+                    self._ra = ra + enc_tracked * solar.SEC_PER_ENC
 
-                time_tracked = dt
-                self._ra = start_ra + enc_tracked * solar.SEC_PER_ENC
-                QtGui.QApplication.processEvents()
-
-            start = start + timedelta(seconds=time_tracked)
+                if self.tracking:
+                    QtGui.QApplication.processEvents()
 
     def return_to_zero(self):
         logging.info('Returning to zero')
