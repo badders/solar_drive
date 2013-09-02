85b969152740a32f1c5f5cdf689aabd498afd132
Tom Badran <tom@badrunner.net>
2013-09-02 12:53:45 +0100

    Update to use new ethernet protocol

--- raw_test/solar_drive.py
+++ raw_test/solar_drive.py
@@ -16,8 +16,7 @@ class SerialApp(QtGui.QApplication):
         self.ui = uic.loadUi('solar_drive.ui')
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_socket.connect((arduino['ip'], arduino['port']))
-
-        time.sleep(2)
+        self.client_socket.setblocking(0)
 
         self.ui.mirrorLeft.pressed.connect(self.mirrorAC)
         self.ui.mirrorRight.pressed.connect(self.mirrorCW)
@@ -28,45 +27,49 @@ class SerialApp(QtGui.QApplication):
         self.timer = QtCore.QTimer()
 
         self.timer.timeout.connect(self.readPort)
-        self.timer.start(50)
+        self.timer.start(300)
 
         self.ui.show()
+        self.ui.raise_()
         self.commands_running = 0
 
     def __del__(self):
         self.client_socket.close()
 
     def readPort(self):
-        while self.client_socket.inWaiting():
-            line = self.client_socket.readline().strip()
-            logging.debug('Received:\t{}'.format(line))
-            self.commands_running -= 1
-            self.ui.textBrowser.append(line)
+        try:
+            data = self.client_socket.recv(1024).strip()
+        except socket.error:
+            return
+        logging.debug('Received:\t{}'.format(data))
+        self.commands_running -= 1
+        self.ui.textBrowser.append(data)
 
     def steps(self):
         return self.ui.steps.value()
 
     def mirrorCW(self):
+        self.commands_running += 1
         self.sendCommand('M', 'C', self.steps())
 
     def mirrorAC(self):
+        self.commands_running += 1
         self.sendCommand('M', 'A', self.steps())
 
     def bodyCW(self):
+        self.commands_running += 1
         self.sendCommand('B', 'C', self.steps())
 
     def bodyAC(self):
+        self.commands_running += 1
         self.sendCommand('B', 'A', self.steps())
 
     def reset(self):
         self.client_socket.write(bytes('R\n'))
 
     def sendCommand(self, motor, direction, steps):
-        if self.commands_running > 0:
-            return
-        self.commands_running += 1
         cmd = bytes('T{}{}{}\n'.format(motor, direction, steps))
-        self.client_socket.write(cmd)
+        self.client_socket.send(cmd)
         logging.debug('Sent:\t{}'.format(cmd))
 
 if __name__ == '__main__':
