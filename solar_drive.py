8a0433552967bcca0d976d7109107f0020cd152a
Tom Badran <tom@badrunner.net>
2013-09-03 12:27:35 +0100

    Find sun before tracking

--- solar_drive.py
+++ solar_drive.py
@@ -110,7 +110,7 @@ class SolarDriverApp(QtGui.QApplication):
             self.telescope.stop_tracking()
             return
         logging.info('Tracking Sun Commencing')
-        #self.find_sun()
+        gself.find_sun()
         self.telescope.start_tracking()
 
     def find_sun(self):
